3a266d7b0fd402eca1eecdc67ba386ba81fee459
Elio1995 <mehmetielio95@gmail.com>
2022-08-03 11:53:08 +0200

    Cuntdown function is working

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,58 @@
-import React from "react"
-import { useState } from 'react'
+import React, { useEffect, useState } from "react"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-  Hello
-    </div>
-  )
+  const calculateTimeLeft = () => {
+    let year = new Date().getFullYear();
+    const difference = +new Date(`08/04/${year}`) - +new Date();
+
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60)
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  });
+
+  const timerComponents = [];
+
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return;
+    }
+  
+    timerComponents.push(
+      <span>
+        {timeLeft[interval]} {interval}{" "}
+      </span>
+    );
+  });
+ 
+  
+    return (
+        <div className="App">
+             <div>
+             {timerComponents.length ? timerComponents : <span>Time's up!</span>}
+            </div>
+        </div>
+    )
 }
-
-export default App
+  
+export default App;
