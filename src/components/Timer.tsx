e554f2ed0a8e1f5958f43499a959e69a4787f24d
Elio1995 <mehmetielio95@gmail.com>
2022-08-03 12:47:11 +0200

    Created a timer component

---
+++ src/components/Timer.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react"
+import '../App.css'
+
+function Timer() {
+
+  const calculateTimeLeft = () => {
+    let year = new Date().getFullYear();
+    const difference = +new Date(`08/04/${year}`) - +new Date();
+
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60)
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  });
+
+  const timerComponents = [];
+
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return;
+    }
+  
+    timerComponents.push(
+      <span className="grid">
+        {timeLeft[interval]} <span >{interval}{" "}</span>
+      </span>
+    );
+  });
+ 
+  
+    return (
+        <div className="App">
+             <div className="grid grid-cols-4 gap-8">
+             {timerComponents.length ? timerComponents : <span>Time's up!</span>}
+            </div>
+        </div>
+    )
+}
+  
+export default Timer;
