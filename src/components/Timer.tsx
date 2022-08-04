1e5cd39f98d2ae87541259a7d64e2794b3dd66dc
Elio1995 <mehmetielio95@gmail.com>
2022-08-04 11:34:31 +0200

    Fixed the type error in timer component

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from "react";
 import "../App.css";
 
+interface time {
+  days: number | string;
+  hours: number | string;
+  minutes: number | string;
+  seconds: number | string;
+}
+
 function Timer() {
   const calculateTimeLeft = () => {
     let year = new Date().getFullYear();
-    const difference = +new Date(`08/04/${year}`) - +new Date();
+    const difference = +new Date(`08/10/${year}`) - +new Date();
 
-    let timeLeft: {
-      days: number | string;
-      hours: number | string;
-      minutes: number | string;
-      seconds: number | string;
-    } = { days: 0, hours: 0, minutes: 0, seconds: 0 };
+    let timeLeft: time = { days: 0, hours: 0, minutes: 0, seconds: 0 };
 
     if (difference > 0) {
       timeLeft = {
@@ -37,14 +39,14 @@ function Timer() {
 
   const timerComponents: any[] = [];
 
-  Object.keys(timeLeft).forEach((interval) => {
-    if (!timeLeft[interval]) {
+  Object.keys(timeLeft).forEach((interval: any) => {
+    if (!timeLeft[interval as keyof time]) {
       return;
     }
 
     timerComponents.push(
-      <span key={timeLeft[interval]} className="grid">
-        {timeLeft[interval]} <span>{interval} </span>
+      <span key={timeLeft[interval as keyof time]} className="grid">
+        {timeLeft[interval as keyof time]} <span>{interval} </span>
       </span>
     );
   });
